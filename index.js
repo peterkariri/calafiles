05fa72a8086dad3562d489ee9da2e7476e9f39e6
Shikuku39 <enshikuku39@gmail.com>
2024-03-26 12:13:08 +0300

    Refactor code for improved performance

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const mysql = require('mysql')
 const session = require('express-session')
 const bcrypt = require('bcrypt')
 const multer = require('multer')
-const xlsx = require('xlsx')
 const dotenv = require('dotenv')
 const nodemailer = require('nodemailer')
-const mysql2 = require('mysql2/promise')
 
 const app = express()
 
@@ -25,14 +23,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'calasoma'
-})
-
-const pool = mysql2.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'calasoma'
+    database: 'calafiles'
 })
 
 const storage = multer.diskStorage({
@@ -48,7 +39,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 app.use((req, res, next) => {
-    res.locals.isLogedIn = (req.session.mwalimuID !== undefined)
+    res.locals.isLogedIn = (req.session.userID !== undefined)
     next()
 })
 
@@ -56,13 +47,13 @@ function loginRequired(req, res) {
     res.locals.isLogedIn || res.redirect('/login')
 }
 
-const offensiveWords = ['FUCK', 'SHIT', 'ASSHOLE', 'BITCH', 'DICK', 'CUNT', 'COCK', 'PUSSY', 'SLUT', 'WHORE', 'FAGGOT', 'MOTHERFUCKER', 'NIGGER', 'RETARD', 'TWAT', 'WANKER', 'ASSWIPE', 'BASTARD', 'DAMN', 'GODDAMN', 'ARSE', 'BOLLOCKS', 'BULLSHIT', 'CRAP', 'JACKASS', 'JERK', 'PISS',  'PRICK', 'SCREW', 'SUCK', 'TITS', 'ASSCLOWN', 'DUMBASS', 'FUCKER', 'SHITHEAD', 'ASSFACE', 'ASSHAT', 'CUM', 'DICKHEAD', 'ASSBAG', 'DIPSHIT', 'FUCKFACE', 'MOTHERFUCKING', 'NIGGA',  'SHITHOLE', 'ASSNUGGET', 'BASTARD', 'BLOWJOB', 'COCKSUCKER', 'CUMSLUT', 'DICKBAG', 'DICKWEED', 'DOUCHEBAG', 'FUCKTARD', 'JACKOFF', 'JIZZ', 'MOTHERFUCK', 'NIGGERS', 'PRICKFACE', 'SHITBAG', 'SHITSTAIN', 'TITFUCK', 'WANKSTAIN']
+const offensiveWords = ['FUCK', 'SHIT', 'ASSHOLE', 'BITCH', 'DICK', 'CUNT', 'COCK', 'PUSSY', 'SLUT', 'WHORE', 'FAGGOT', 'MOTHERFUCKER', 'NIGGER', 'RETARD', 'TWAT', 'WANKER', 'ASSWIPE', 'BASTARD', 'DAMN', 'GODDAMN', 'ARSE', 'BOLLOCKS', 'BULLSHIT', 'CRAP', 'JACKASS', 'JERK', 'PISS',  'PRICK', 'SCREW', 'SUCK', 'TITS', 'ASSCLOWN', 'DUMBASS', 'FUCKER', 'SHITHEAD', 'ASSFACE', 'ASSHAT', 'CUM', 'DICKHEAD', 'ASSBAG', 'DIPSHIT', 'FUCKFACE', 'MOTHERFUCKING', 'NIGGA',  'SHITHOLE', 'ASSNUGGET', 'BASTARD', 'BLOWJOB', 'COCKSUCKER', 'CUMSLUT', 'DICKBAG', 'DICKWEED', 'DOUCHEBAG', 'FUCKTARD', 'JACKOFF', 'JIZZ', 'MOTHERFUCK', 'NIGGERS', 'PRICKFACE', 'SEX', 'SHITBAG', 'SHITSTAIN', 'TITFUCK', 'WANKSTAIN']
 
 function generateid() {
     const characters = 'ABCDEFGHKMNPQRSTUVWXYZ23456789'
     let genId
     do {
-        const randomCharacters = Array.from({ length: 5 }, () => characters.charAt(Math.floor(Math.random() * characters.length)))
+        const randomCharacters = Array.from({ length: 3 }, () => characters.charAt(Math.floor(Math.random() * characters.length)))
         genId = randomCharacters.join('')
     } while (offensiveWords.some(word => genId.includes(word)))
     
@@ -114,49 +105,50 @@ const sendMail = (data) => {
 }
 
 app.get('/', (req, res) => {
-    if (req.session.modesession) {
-        res.render('index')
-        return
-    }
-    let newId = ''
-    let sql = 'SELECT * FROM sessions WHERE sessionid = ?'
-    do {
-        newId = generatesession()
-    } while (checkIfIdExists(newId, sql))
-    req.session.modesession = newId
-    let sqlsession = 'INSERT INTO sessions (sessionid) VALUES (?)'
-    connection.query(sqlsession, [newId], (error, results) => {
-        res.render('index')
-    })
+    res.render('index')
+    // if (req.session.modesession) {
+    //     res.render('index')
+    //     return
+    // }
+    // let newId = ''
+    // let sql = 'SELECT * FROM sessions WHERE sessionid = ?'
+    // do {
+    //     newId = generatesession()
+    // } while (checkIfIdExists(newId, sql))
+    // req.session.modesession = newId
+    // let sqlsession = 'INSERT INTO sessions (sessionid) VALUES (?)'
+    // connection.query(sqlsession, [newId], (error, results) => {
+    //     res.render('index')
+    // })
 })
 
 app.get('/signup', (req, res) => {
-    const mwalimu = {
+    const user = {
         name: 'SHIT',
         email: 'enshikuku@gmail.com',
         password: '2',
         confirmpassword: '2'
     }
-    res.render('signup', { error: false, mwalimu: mwalimu })
+    res.render('signup', { error: false, user: user })
 })
 
 app.post('/signup', async(req, res) => {
-    const mwalimu = {
+    const user = {
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
         confirmpassword: req.body.confirmpassword
     }
-    if (mwalimu.password === mwalimu.confirmpassword) {
-        let sql = 'SELECT * FROM mwalimu WHERE email = ?'
-        connection.query(sql, [mwalimu.email], (error, results) => {
+    if (user.password === user.confirmpassword) {
+        let sql = 'SELECT * FROM user WHERE email = ?'
+        connection.query(sql, [user.email], (error, results) => {
             if (results.length > 0) {
                 let message = 'An account exists with that email number!'
-                mwalimu.email = ''
-                res.render('signup', { error: true, message: message, mwalimu: mwalimu })
+                user.email = ''
+                res.render('signup', { error: true, message: message, user: user })
             } else {
                 let newId = ''
-                let sql = 'SELECT * FROM mwalimu WHERE id = ?'
+                let sql = 'SELECT * FROM user WHERE id = ?'
                 do {
                     newId = generateid()
                 } while (checkIfIdExists(newId, sql))
@@ -169,19 +161,19 @@ app.post('/signup', async(req, res) => {
                 req.session.otp = OTP
                 let otpsql2 = 'INSERT INTO otp (otpcode, email) VALUES (?, ?)'
 
-                connection.query(otpsql2, [OTP, mwalimu.email], (error, results) => {
+                connection.query(otpsql2, [OTP, user.email], (error, results) => {
                     if (error) {
                         console.error('Error inserting OTP:', error)
                     } else {
                         const mailData = {
                             from: process.env.EMAIL,
-                            to: mwalimu.email,
-                            subject: 'Welcome to CALASOMA - Your Personalized Learning Platform',
-                            text: `Dear ${mwalimu.name},\n\nWelcome to CALASOMA! Your One-Time Password (OTP) for account verification is ${OTP}. This OTP will expire in 10 minutes.\n\nHappy learning!\n\nBest regards,\nThe CALASOMA Team`
+                            to: user.email,
+                            subject: 'Welcome to calafiles - Your Personalized Learning Platform',
+                            text: `Dear ${user.name},\n\nWelcome to calafiles! Your One-Time Password (OTP) for account verification is ${OTP}. This OTP will expire in 10 minutes.\n\nHappy learning!\n\nBest regards,\nThe calafiles Team`
                         }
                         sendMail(mailData)
                         console.log('OTP inserted')
-                        res.render('otp', { error: false, mwalimu: mwalimu, newId: newId})
+                        res.render('otp', { error: false, user: user, newId: newId})
                         setTimeout(() => {
                             const updateSql = 'UPDATE otp SET used = true WHERE otpcode = ?'
                             connection.query(updateSql, [OTP], (updateError, updateResults) => {
@@ -198,13 +190,13 @@ app.post('/signup', async(req, res) => {
         })
     } else {
         let message = 'Passwords don\'t match!'
-        mwalimu.confirmpassword = ''
-        res.render('signup', { error: true, message: message, mwalimu: mwalimu })
+        user.confirmpassword = ''
+        res.render('signup', { error: true, message: message, user: user })
     }
 })
 
 app.post('/verify-otp', (req, res) => {
-    const mwalimu = {
+    const user = {
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
@@ -212,50 +204,50 @@ app.post('/verify-otp', (req, res) => {
         otp: req.body.otp
     }
     let sql = 'SELECT * FROM otp WHERE otpcode = ? AND used = false'
-    connection.query(sql, [mwalimu.otp], (error, results) => {
+    connection.query(sql, [user.otp], (error, results) => {
         if (results.length > 0) {
-            let sql = 'INSERT INTO mwalimu (name, email, password, id) VALUES (?, ?, ?, ?)'
-            bcrypt.hash(mwalimu.password, 10, (err, hash) => {
-                connection.query(sql, [mwalimu.name, mwalimu.email, hash, mwalimu.id], (error, results) => {
-                    req.session.mwalimuID = mwalimu.id
+            let sql = 'INSERT INTO user (name, email, password, id) VALUES (?, ?, ?, ?)'
+            bcrypt.hash(user.password, 10, (err, hash) => {
+                connection.query(sql, [user.name, user.email, hash, user.id], (error, results) => {
+                    req.session.userID = user.id
                     res.redirect('/dashboard')
                 })
             })
         } else {
             let message = 'Invalid OTP!'
-            res.render('otp', { error: true, message: message, mwalimu: mwalimu, newId: mwalimu.id })
+            res.render('otp', { error: true, message: message, user: user, newId: user.id })
         }
     })
 })
 
 app.get('/login', (req, res) => {
-    const mwalimu = {
+    const user = {
         email: '',
         password: ''
     }
-    res.render('login', { error: false, mwalimu: mwalimu })
+    res.render('login', { error: false, user: user })
 })
 
 app.post('/login', (req, res) => {
-    const mwalimu = {
+    const user = {
         email: req.body.email,
         password: req.body.password
     }
-    let sql = 'SELECT * FROM mwalimu WHERE email = ?'
-    connection.query(sql, [mwalimu.email], (error, results) => {
+    let sql = 'SELECT * FROM user WHERE email = ?'
+    connection.query(sql, [user.email], (error, results) => {
         if (results.length > 0) {
-            bcrypt.compare(mwalimu.password, results[0].password, (error, passwordMatches) => {
+            bcrypt.compare(user.password, results[0].password, (error, passwordMatches) => {
                 if (passwordMatches) {
-                    req.session.mwalimuID = results[0].id
+                    req.session.userID = results[0].id
                     res.redirect('/dashboard')
                 } else {
                     let message = 'Incorrect password!'
-                    res.render('login', { error: true, message: message, mwalimu: mwalimu })
+                    res.render('login', { error: true, message: message, user: user })
                 }
             })
         } else {
             let message = 'Account does not exist. Please create one'
-            res.render('login', { error: true, message: message, mwalimu: mwalimu })
+            res.render('login', { error: true, message: message, user: user })
         }
     })
 })
@@ -263,43 +255,6 @@ app.post('/login', (req, res) => {
 app.get('/dashboard', (req, res) => {
     loginRequired(req, res)
     res.render('dashboard')
-})
-
-app.get('/upload', (req, res) => {
-    // loginRequired(req, res)
-    res.render('upload', {error: false})
-})
-
-app.post('/uploadworkbook', upload.single('file'), async (req, res) => {
-    let connection
-    try {
-        const workbook = xlsx.readFile(req.file.path)
-        const worksheet = workbook.Sheets[workbook.SheetNames[0]]
-        const data = xlsx.utils.sheet_to_json(worksheet, { header: 1 })
-
-        connection = await pool.getConnection()
-        await connection.beginTransaction()
-
-        for (let row of data) {
-            const [Name, AdmissionNo, Subject1, Subject2, Subject3, Subject4, Subject5, Subject6] = row
-            try {
-                await connection.query(
-                    'INSERT INTO StudentData (Name, AdmissionNo, Subject1, Subject2, Subject3, Subject4, Subject5, Subject6) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-                    [Name, AdmissionNo, Subject1, Subject2, Subject3, Subject4, Subject5, Subject6]
-                )
-            } catch (error) {
-                console.error('Error occurred during insertion:', error)
-            }
-        }
-
-        await connection.commit()
-        res.send('Data uploaded successfully')
-    } catch (error) {
-        console.error('Error occurred during data insertion:', error)
-        res.status(500).send('Error occurred during data insertion')
-    } finally {
-        if (connection) connection.release()
-    }
 })
 
 app.get('/logout', (req, res) => {
@@ -313,7 +268,7 @@ app.get('*', (req, res) => {
     res.render('404')
 })
 
-const PORT = process.env.PORT || 3004
+const PORT = process.env.PORT || 311
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
