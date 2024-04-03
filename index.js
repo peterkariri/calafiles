a637d192b4853a58be3201af0d9307db245becd1
Shikuku39 <enshikuku39@gmail.com>
2024-04-03 23:43:32 +0300

    Fix typo in welcome email message

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ app.post('/signup', async(req, res) => {
                             from: process.env.EMAIL,
                             to: user.email,
                             subject: 'Welcome to CALAFILES - Your Personalized Learning Platform',
-                            text: `Dear ${user.name},\n\nWelcome to calafiles! Your One-Time Password (OTP) for account verification is ${OTP}. This OTP will expire in 10 minutes.\n\nHappy learning!\n\nBest regards,\nThe CALAFILES Team`
+                            text: `Dear ${user.name},\n\nWelcome to CALAFILES! Your One-Time Password (OTP) for account verification is ${OTP}. This OTP will expire in 10 minutes.\n\nHappy learning!\n\nBest regards,\nThe CALAFILES Team`
                         }
                         sendMail(mailData)
                         console.log('OTP inserted')
