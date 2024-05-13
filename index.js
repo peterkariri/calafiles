c366cf09c861298dc0712e0df8a102cd268e3e50
Shikuku39 <enshikuku39@gmail.com>
2024-05-13 08:44:36 +0300

    chore: Update PORT to 4050 in index.js

--- index.js
+++ index.js
@@ -845,7 +845,7 @@ app.get('*', (req, res) => {
     res.render('404')
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4050
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
